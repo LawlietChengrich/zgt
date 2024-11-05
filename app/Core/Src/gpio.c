5d5da0eddf149a27069dad18612ed71310c8ebe4
junye.zheng <zhengjunye123@126.com>
2024-11-05 11:39:04 +0800

    zgt6：修改延时函数bug

--- app/Core/Src/gpio.c
+++ app/Core/Src/gpio.c
@@ -72,7 +72,7 @@ void dh_gpio_1pluse(uint16_t ms, uint16_t gpio_num)
 #else
     LL_GPIO_SetOutputPin(dh_gpio_struct[gpio_num].group, dh_gpio_struct[gpio_num].pin);
 #endif
-    gpio_delay_us[gpio_num] = ms;
+    gpio_delay_us[gpio_num] = ms*(1000/SYSTICK_HANDLE_US);
   }
 }
 
